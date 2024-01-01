5ab7c425928cf193098dfe946ce409fb5f9155ec
kalesd11 <surajkales111@gmail.com>
2024-01-01 21:24:19 +0530

    added projects

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -161,19 +161,19 @@ const Navbar = () => {
             Dashboard
           </a>
           <a
-            href="/"
+            href="/skills"
             className="text-gray-600 nav hover:bg-blue-600 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
           >
             Skills
           </a>
           <a
-            href="/"
+            href="/projects"
             className="text-gray-600 nav hover:bg-blue-600 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
           >
             Projects
           </a>
           <a
-            href="/"
+            href="/contact"
             className="text-gray-600 nav hover:bg-blue-600 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
           >
             Connect Me
